01962e3fd46e4578d33eb30bce251f16d451178a
JayWooKJ <natcho9010@gmail.com>
2019-04-10 11:09:25 +0900

    projectcontent 수정 끝남

--- src/main/java/com/team/sprout/controllers/ProjectDetailCotroller.java
+++ src/main/java/com/team/sprout/controllers/ProjectDetailCotroller.java
@@ -2,8 +2,6 @@ package com.team.sprout.controllers;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,9 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.team.sprout.dao.MemberRepository;
 import com.team.sprout.dao.ProjectMemberRepository;
 import com.team.sprout.dao.WorkSpaceRepository;
-import com.team.sprout.vo.MainProject;
 import com.team.sprout.vo.Member;
-import com.team.sprout.vo.ProjectMember;
 
 @Controller
 public class ProjectDetailCotroller {
@@ -27,11 +23,33 @@ public class ProjectDetailCotroller {
 	MemberRepository memRepo;	
 	List<Member> projectMembers;
 	
+	final String upLoadPath = "/sproutProjectFileManager";
+	//기획... log를 저장해서 이곳에서 통계표를 만든다
+	
+	//통계표는 log 행동한 데이터의 종류를 숫자로 구분해서 나눈다. 거치는 controller 저장
+	//등록 완료 중간저장 6시면 자동저장, 일일 진행사항 = 그 날 업무 할당량 달성정도
+	//그 사람이 했다고 제출하면 관리자는 o x를 체크한다.
+	//
+	
+	
+	//파일 
+	
+	/*// 파일 매니저 접속
+	@RequestMapping(value = "/fileManager", method = RequestMethod.GET)
+	public String whiteBoard(Model model, String postitNumFromProjectNum) {
+		if (postitNumFromProjectNum != null) {
+			model.addAttribute("projectNum", postitNumFromProjectNum);
+		}
+		return "project/fileManager";
+	}*/
+	
+	
 	
 	@RequestMapping(value ="/detailPage", method = RequestMethod.GET)
-	public String project_detail() {
+	public String project_detail(Model model) {
 
-	
+		model.addAttribute("projectNum", "");
+		
 		return "project/detailPage";
 	}
 
